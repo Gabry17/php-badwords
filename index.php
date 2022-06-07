c6d6a4cf08ed9de61fe5f147543cf22fa2a8b01a
Gabry17 <gbianchi528@gmail.com>
2022-06-07 16:49:51 +0200

    censurato parola nel testo e aggiornato stampato lunghezza

--- index.php
+++ index.php
@@ -10,15 +10,34 @@
     <h2>creo e visualizzo variabile</h2>
     <p>
         <?php 
-        $text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto atque illum eaque in quas quidem alias ab cumque laborum eos, neque aperiam harum expedita ullam quasi. Quia amet pariatur blanditiis!";
+        $text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem Architecto atque illum eaque in quas quidem alias ab cumque laborum eos, Lorem neque aperiam harum expedita ullam quasi. Quia amet pariatur blanditiis!";
         echo $text;
         ?>
     </p>
 
     <h2>stampo la lunghezza</h2>
-    <?php 
-    echo "La mia lunghezza è: " . strlen($text);
+    <p>
+        <?php 
+        echo "La mia lunghezza è: " . strlen($text);
+        ?>
+    </p>
+
+    <h2>Parola da censurare</h2>
+    <p>
+        <?php
+        $badWords = $_GET["words"];
+        echo $badWords;
     ?>
+    </p>
+
+    <h2>Censurare parola</h2>
+    <p>
+        <?php
+        $censor = str_replace($badWords, "***", $text);
+        echo $censor;
+        echo "<br> La mia lunghezza è: " . strlen($censor);
+        ?>
+    </p>
 
 </body>
 </html>
