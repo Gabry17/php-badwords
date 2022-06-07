2b12d61127f33c74090d102ea3436da11d414509
Gabry17 <gbianchi528@gmail.com>
2022-06-07 16:24:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php 
+$text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto atque illum eaque in quas quidem alias ab cumque laborum eos, neque aperiam harum expedita ullam quasi. Quia amet pariatur blanditiis!";
+echo $text;
+?>
